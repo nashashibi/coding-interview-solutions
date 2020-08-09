f7a1a63f892eb1bac4b611b042681208df8ae670
Mohammad <moe.nashashibi@gmail.com>
2020-08-10 00:43:48 +0100

    feat: add add_one

---
+++ add_one.js
@@ -0,0 +1,36 @@
+input = [0]
+input = [9]
+input = [0, 0, 0]
+input = [9, 9, 9]
+input = [2, 3, 4]
+input = [2, 9, 9]
+
+function add_one (nums) {
+  let i = nums.length - 1
+  let done = false
+  
+  
+  while (!done) {
+    const n = nums[i]
+        
+    if (n === 9) {
+      nums[i] = 0
+      // to add 1 at the beginnning if all nums are 9
+      if (i === 0) {
+        nums.unshift(1)
+        done = true
+      }
+    } else {
+      nums[i] += 1
+      done = true
+    }
+    
+    i--
+  }
+    
+  return nums
+}
+
+res = add_one(input)
+
+console.log('output:', res)
